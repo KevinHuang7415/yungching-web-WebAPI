ea1b3f1741301ca9dbc5fa94c58dfeaf4c341564
Kevin Huang <goliath7415@gmail.com>
2017-12-01 22:22:26 +0800

    Use repository pattern for data access

---
+++ yungching-web-WebAPI/Repository/Repository.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace yungching_web_WebAPI.Repository
+{
+    public class Repository<TEntity> : IRepository<TEntity>
+        where TEntity : class
+    {
+        private DbContext Context { get; set; }
+
+        public Repository(DbContext inContext) => Context = inContext;
+
+        public void Create(TEntity entity) => Context.Set<TEntity>().Add(entity);
+
+        public TEntity Read(Expression<Func<TEntity, bool>> predicate) =>
+            Context.Set<TEntity>().Where(predicate).FirstOrDefault();
+
+        public IQueryable<TEntity> Reads() => Context.Set<TEntity>().AsQueryable();
+
+        public void Update(TEntity entity) => Context.Entry(entity).State = EntityState.Modified;
+
+        public void Update(TEntity entity, Expression<Func<TEntity, object>>[] updateProperties)
+        {
+            Context.Configuration.ValidateOnSaveEnabled = false;
+
+            Context.Entry(entity).State = EntityState.Unchanged;
+
+            if (updateProperties != null)
+            {
+                foreach (var property in updateProperties)
+                {
+                    Context.Entry(entity).Property(property).IsModified = true;
+                }
+            }
+        }
+
+        public void Delete(TEntity entity) => Context.Entry(entity).State = EntityState.Deleted;
+
+        public void SaveChanges()
+        {
+            Context.SaveChanges();
+
+            if (Context.Configuration.ValidateOnSaveEnabled == false)
+            {
+                Context.Configuration.ValidateOnSaveEnabled = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing && Context != null)
+            {
+                Context.Dispose();
+                Context = null;
+            }
+        }
+    }
+}
