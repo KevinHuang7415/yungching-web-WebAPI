832d8a72bccecf9edbac12f34f62577a77d8c4de
Kevin Huang <goliath7415@gmail.com>
2017-12-01 15:05:35 +0800

    Create ASP.NET Web API application

---
+++ yungching-web-WebAPI.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using yungching_web_WebAPI;
+using yungching_web_WebAPI.Controllers;
+
+namespace yungching_web_WebAPI.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // 排列
+            HomeController controller = new HomeController();
+
+            // 作用
+            ViewResult result = controller.Index() as ViewResult;
+
+            // 判斷提示
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Home Page", result.ViewBag.Title);
+        }
+    }
+}
---
+++ yungching-web-WebAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace yungching_web_WebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
